ccac1ea06d668f880653735a0fc5ad6f9b0e055b
Chelsea <chelsea.battell@gmail.com>
2018-11-11 14:29:41 -0500

    Fixed tests.

--- TreeTest/scripts/buildtree.test.js
+++ TreeTest/scripts/buildtree.test.js
@@ -1,21 +1,27 @@
-// const { buildNode } = require('./buildtree')
+const { buildNode } = require('./buildtree')
 
-// // use .toEqual for deep equality, otherwise .toBe
+// use .toEqual for deep equality, otherwise .toBe
 
-// test('test initializing a leaf with the `buildNode` function',
-//     () =>
-//     {
-//         var leafProp = "some prop";
-//         expect(buildNode(leafProp)).toEqual(
-//             {
-//                 proposition: leafProp,
-//                 children: null,
-//                 leftContent: "",
-//                 rightContent: "",
-//                 rowStart: null,
-//                 rowEnd: null,
-//                 colStart: null,
-//                 colEnd: null
-//             }
-//         )
-//     })
+test('test initializing a leaf with the `buildNode` function',
+    () =>
+    {
+        var leafProp = "some prop";
+        var leafName = "some name";
+        expect(buildNode(leafProp, leafName)).toEqual(
+            {
+                proposition: leafProp,
+                name: leafName,
+                children: null,
+                leftContent: "",
+                rightContent: "",
+                rowStart: null,
+                rowEnd: null,
+                colStart: null,
+                colEnd: null,
+                x: -1,
+                y: -1,
+                mod: -1,
+                previousSibling: null
+            }
+        )
+    })
