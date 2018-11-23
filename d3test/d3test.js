fcfb01fd97cd3b8ea41987c7f1a97d33f191edaa
Chelsea <chelsea.battell@gmail.com>
2018-11-22 23:15:06 -0500

    Removed timeout functions.

--- d3test/d3test.js
+++ d3test/d3test.js
@@ -546,115 +546,25 @@ window.onload = () =>
 	});
 }
 
-// setTimeout(() =>
-// {
-// 	// add something like `onpageready`
-// 	MathJax.Hub.Register.StartupHook("End", function() {
-// 		setTimeout(() => {
-// 			svgtree.selectAll(`${webvars.nodeContainerTag}.${webvars.nodeContainerClass}`).each(function(){
-// 				var self = d3.select(this),
-// 					g = self.select(`${webvars.nodeTextTag} > span > svg`);
-
-// 				if (g.node())
-// 				{
-// 					g.remove();
-// 					self.append(webvars.texContainerTag)
-// 						.classed(webvars.texContainerClass, true)
-// 						.attr('width', '100%')
-// 						.style('overflow', 'visible')
-// 						.on('click', node_onclick)
-// 						.append(function(){
-// 							return g.node();
-// 						})
-// 						.attr('width', '50%')
-// 						.attr('x', '-25%');
-// 				}
-// 			});
-
-// 		}, 1);
-// 	});
-
-// 	// MathJax.Hub.Queue(["Typeset", MathJax.Hub, svgtree.node()]);
-
-// 	setTimeout(() =>
-// 	{
-// 		PostRender();
-// 	}, 1);
-
-// 	MathJax.Hub.Typeset();
-// 	// MathJax.Hub.Queue(["Typeset", MathJax.Hub]);
-
-// }, 1000);
-
-
-// setTimeout(() => {
-// 	svgtree.selectAll(`${webvars.ruleTextContainerTag}.${webvars.ruleTextClass}`).each(function(){
-// 		var self = d3.select(this),
-// 			g = self.select(`${webvars.nodeTextTag} > span > svg`);
-
-// 		if (g.node())
-// 		{
-// 			g.remove();
-// 			self.append(webvars.texContainerTag)
-// 				.classed(webvars.texContainerClass, true)
-// 				.attr('width', '100%')
-// 				.style('overflow', 'visible')
-// 				.on('click', node_onclick)
-// 				.append(function(){
-// 					return g.node();
-// 				})
-// 				// .attr('width', '50%')
-// 				// .attr('x', '-25%');
-// 		}
-// 	});
-
-// }, 1500);
-
 function MathJaxRuleText()
 {
-	// setTimeout(() =>
-	// {
-		svgtree.selectAll(`${webvars.ruleTextContainerTag}.${webvars.ruleTextClass}`).each(function(){
-			var self = d3.select(this),
-				g = self.select(`${webvars.nodeTextTag} > span > svg`);
-			debugger;
-			if (g.node())
-			{
-				g.remove();
-				self.append(webvars.texContainerTag)
-					.classed(webvars.texContainerClass, true)
-					.attr('width', '100%')
-					.style('overflow', 'visible')
-					.on('click', node_onclick)
-					.append(function(){
-						return g.node();
-					})
-					// .attr('width', '50%')
-					// .attr('x', '-25%');
-			}
-		});
-
-// 		MathJax.Hub.Register.StartupHook("End", function() {
-
-// 				svgtree.selectAll(`${webvars.ruleTextContainerTag}.${webvars.ruleTextClass}`).each(function(){
-// 					var self = d3.select(this),
-// 						g = self.select(`${webvars.nodeTextTag} > span > svg`);
-// console.log("Doing it.")
-// 					if (g.node())
-// 					{
-// 						g.remove();
-// 						self.append(webvars.texContainerTag)
-// 							.classed(webvars.texContainerClass, true)
-// 							.attr('width', '100%')
-// 							.style('overflow', 'visible')
-// 							.on('click', node_onclick)
-// 							.append(function(){
-// 								return g.node();
-// 							})
-// 							// .attr('width', '50%')
-// 							// .attr('x', '-25%');
-// 					}
-// 				});
-		// });
-	// }, 1500); // this timeout length must be after previous one
+	svgtree.selectAll(`${webvars.ruleTextContainerTag}.${webvars.ruleTextClass}`).each(function(){
+		var self = d3.select(this),
+			g = self.select(`${webvars.nodeTextTag} > span > svg`);
+		// debugger;
+		if (g.node())
+		{
+			g.remove();
+			self.append(webvars.texContainerTag)
+				.classed(webvars.texContainerClass, true)
+				.attr('width', '100%')
+				.style('overflow', 'visible')
+				.on('click', node_onclick)
+				.append(function(){
+					return g.node();
+				})
+				// .attr('width', '50%')
+				// .attr('x', '-25%');
+		}
+	});
 }
