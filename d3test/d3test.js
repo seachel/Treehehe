cb4f11cb8200b924c1917e75b3b7bd0d101e2e0a
Chelsea <chelsea.battell@gmail.com>
2018-11-26 23:26:38 -0500

    Moved tree examples above traversal functions.

--- d3test/d3test.js
+++ d3test/d3test.js
@@ -72,6 +72,61 @@ TreeDataMaker = (function()
 })();
 
 
+// ---------- Examples
+
+const TreeExamples = (function()
+{
+	let data = TreeDataMaker.makeProofTreeNode("$x \\rightarrow y$",
+		[
+			TreeDataMaker.makeProofTreeNode("$\\forall x, P \; x$"),
+			TreeDataMaker.makeProofTreeNode("1 + 2 + 3 = :)",
+			[
+				TreeDataMaker.makeProofTreeNode("$x \\supset y$"),
+				TreeDataMaker.makeProofTreeNode("$\\wedge$")
+			]),
+			TreeDataMaker.makeProofTreeNode("$\\forall x$")
+		],
+	"A right",
+	"A left");
+
+	let treeExample1 = TreeDataMaker.makeProofTreeNode("$(p \\wedge r) \\rightarrow (q \\wedge s)$",
+		[
+			TreeDataMaker.makeProofTreeNode("$q \\wedge s$",
+			[
+				TreeDataMaker.makeProofTreeNode("$q$",
+				[
+					TreeDataMaker.makeProofTreeNode("$p$",
+					[
+						TreeDataMaker.makeProofTreeNode("$p \\wedge r$", [], "$u$")
+					],
+					"$\\wedge_{E_1}$"),
+					TreeDataMaker.makeProofTreeNode("$p \\rightarrow q$", null)
+				],
+				"$\\rightarrow_E$"),
+				TreeDataMaker.makeProofTreeNode("$s$",
+				[
+					TreeDataMaker.makeProofTreeNode("$r$",
+					[
+						TreeDataMaker.makeProofTreeNode("$p \\wedge u$", [], "$u$")
+					],
+					"$\\wedge_{E_2}$"),
+					TreeDataMaker.makeProofTreeNode("$r \\rightarrow s$", null)
+				],
+				"$\\wedge_E$")
+			],
+			"$\\wedge_I$")
+		],
+		"$\\rightarrow_{I^u}$");
+
+	return {
+		ex1: data,
+		natded_ex1: treeExample1
+	}
+})();
+
+let selectedTree = TreeExamples.natded_ex1;
+
+
 // ---------- Tree Traversal
 
 function visitNodes_postOrder(rootNode, nodeCallback, nodeCallbackArgs = [], iterationHeight = 0, childCallback = null, childCallbackArgs = [])
@@ -124,60 +179,6 @@ function visitNodes_preOrder(rootNode, nodeCallback = null, nodeCallbackArgs = [
 	iterationHeight++;
 }
 
-
-// ---------- Examples
-
-var TreeExamples = (function()
-{
-	let data = TreeDataMaker.makeProofTreeNode("$x \\rightarrow y$",
-		[
-			TreeDataMaker.makeProofTreeNode("$\\forall x, P \; x$"),
-			TreeDataMaker.makeProofTreeNode("1 + 2 + 3 = :)",
-			[
-				TreeDataMaker.makeProofTreeNode("$x \\supset y$"),
-				TreeDataMaker.makeProofTreeNode("$\\wedge$")
-			]),
-			TreeDataMaker.makeProofTreeNode("$\\forall x$")
-		],
-	"A right",
-	"A left");
-
-	let treeExample1 = TreeDataMaker.makeProofTreeNode("$(p \\wedge r) \\rightarrow (q \\wedge s)$",
-		[
-			TreeDataMaker.makeProofTreeNode("$q \\wedge s$",
-			[
-				TreeDataMaker.makeProofTreeNode("$q$",
-				[
-					TreeDataMaker.makeProofTreeNode("$p$",
-					[
-						TreeDataMaker.makeProofTreeNode("$p \\wedge r$", [], "$u$")
-					],
-					"$\\wedge_{E_1}$"),
-					TreeDataMaker.makeProofTreeNode("$p \\rightarrow q$", null)
-				],
-				"$\\rightarrow_E$"),
-				TreeDataMaker.makeProofTreeNode("$s$",
-				[
-					TreeDataMaker.makeProofTreeNode("$r$",
-					[
-						TreeDataMaker.makeProofTreeNode("$p \\wedge u$", [], "$u$")
-					],
-					"$\\wedge_{E_2}$"),
-					TreeDataMaker.makeProofTreeNode("$r \\rightarrow s$", null)
-				],
-				"$\\wedge_E$")
-			],
-			"$\\wedge_I$")
-		],
-		"$\\rightarrow_{I^u}$");
-
-	return {
-		ex1: data,
-		natded_ex1: treeExample1
-	}
-})();
-
-let selectedTree = TreeExamples.natded_ex1;
 
 // ------------------------------ D3 ------------------------------
 
